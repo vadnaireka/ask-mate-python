d0960788675280dce0644680cab52a5ea31ea9ac
vadnaireka <budaikreativ@gmail.com>
2019-01-14 14:17:26 +0100

    /list: table header server.py: @app.route("/")

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def route_list():
+    with open ('sample_data/question.csv', 'r') as file:
+        data_file = csv.DictReader(file)
+        data = list(data_file)
+    return render_template('list.html', data=data)
+
 
 
 if __name__ == '__main__':
