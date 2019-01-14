5f9c0a151546347bc3d87d0c4d83ca0d96cea5bf
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-01-14 15:14:06 +0100

    last commit of the day

--- server.py
+++ server.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def route_list():
-    with open ('/sample_data/question.csv', 'r') as file:
+    with open ('sample_data/question.csv', 'r') as file:
         data_file = csv.DictReader(file)
         data = list(data_file)
     return render_template('list.html', data=data)
 
 
 
-@app.route('/add-question', methods=['POST'])
+@app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
     if request.method == 'POST':
         id += 1
@@ -28,7 +28,7 @@ def add_question():
         with open('sample_data/question.csv', 'a') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writerow({'id': id, 'submission_time': submission_time, 'view_number': view_number, 'vote_number': vote_number,
-                             'title': title, 'message': message, 'image': image})
+                         'title': title, 'message': message, 'image': image})
     return render_template('add_question.html', )
 
 
