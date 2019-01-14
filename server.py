08dd5ccfe8750918a7f975d8d6bdbb2b35b6ff44
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-01-14 14:01:25 +0100

    Add question html and route added

--- server.py
+++ server.py
@@ -8,5 +8,9 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/add-question')
+def add_questuion():
+    return 'You will be able to add questions here'
+
 if __name__ == '__main__':
     app.run()
