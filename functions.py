73ca9c4029c8a35a6eb6d3281950444c43a04a0c
vadnaireka <budaikreativ@gmail.com>
2019-01-28 13:59:14 +0100

    sql - route_question

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import csv
+import database_common
 
 fieldnames_question = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 fieldnames_answer = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
@@ -31,3 +32,32 @@ def delete_question_and_answers_by_user_id(id):
         writer = csv.DictWriter(file, fieldnames=fieldnames_answer)
         writer.writeheader()
         writer.writerows(new_data)
+
+
+@database_common.connection_handler
+def list_questions(cursor):
+    cursor.execute("""
+                    SELECT title, id FROM question
+                    """)
+    data = cursor.fetchall()
+    return data
+
+
+@database_common.connection_handler
+def display_question(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM question
+                    where id = %s
+                    """, id)
+    question_data = cursor.fetchall()
+    return question_data
+
+
+@database_common.connection_handler
+def display_answer(cursor, id):
+    cursor.execute("""
+                    SELECT * FROM answer
+                    where question_id = %s
+                    """, id)
+    answer_data = cursor.fetchall()
+    return answer_data
