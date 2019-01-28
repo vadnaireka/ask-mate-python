663f47c286263f05cfea2b695a08342d0cf3e4e3
Dávid Szabó <drszabodavid@gmail.com>
2019-01-29 00:04:22 +0100

    add comment to question

--- database_common.py
+++ database_common.py
@@ -8,10 +8,10 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
+    user_name = 'david'
+    password = 'david'
+    host = 'localhost'
+    database_name = 'aksmdb'
 
     env_variables_defined = user_name and password and host and database_name
 
