183f466e6d9ce234a541de4ef31468e658cfc87c
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-01-31 10:23:11 +0100

    homepage only listing 5 questions, clicking on the button reveal all the questions and it's ordered by sub time, descending

--- functions.py
+++ functions.py
@@ -32,6 +32,7 @@ def delete_question_by_question_id(cursor, id):
 def list_questions(cursor):
     cursor.execute("""
                     SELECT * FROM question
+                    ORDER BY submission_time DESC 
                     """)
     data = cursor.fetchall()
     return data
@@ -41,6 +42,7 @@ def list_questions(cursor):
 def list_five_questions(cursor):
     cursor.execute("""
                     SELECT * FROM question
+                    ORDER BY submission_time DESC 
                     LIMIT 5
                     """)
     data = cursor.fetchall()
