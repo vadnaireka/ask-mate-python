67a181850a1b1c8b0951bbe3ef01e3da0cb89754
Dávid Szabó <drszabodavid@gmail.com>
2019-01-31 14:16:40 +0100

    definitive edition

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import database_common
+from psycopg2 import sql
 
 fieldnames_question = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 fieldnames_answer = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
@@ -61,7 +62,7 @@ def delete_comment_by_question_id(cursor, id):
     cursor.execute("""
                     delete from comment
                     where id = %(id)s
-                    """, {'id': id})\
+                    """, {'id': id})
 
 
 @database_common.connection_handler
@@ -102,11 +103,11 @@ def list_five_questions(cursor):
 
 
 @database_common.connection_handler
-def sort_questions(cursor, column, order):
-    cursor.execute("""
-                    SELECT * FROM question
-                    order by %(column)s %(order)s
-                    """, {'column': column, 'order': order})
+def sort_questions(cursor, order_by, order_direction):
+    cursor.execute(sql.SQL("""SELECT *  FROM question
+                              ORDER BY {order_by} {order_direction}
+                """.format(order_by=order_by,
+                            order_direction=order_direction)))
     data = cursor.fetchall()
     return data
 
--- server.py
+++ server.py
@@ -10,9 +10,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def route_list():
     if request.method == 'POST':
-        column = request.form['column']
-        order = request.form['order']
-        data = functions.sort_questions(column, order)
+        order_by = request.form['column']
+        order_direction = request.form['order']
+        data = functions.sort_questions(order_by, order_direction)
+        url = 'home'
     if request.method == 'GET':
         url = 'home'
         data = functions.list_five_questions()
@@ -44,7 +45,8 @@ def route_new_answer(id):
         question = questions[0]
         return render_template('new_answer.html', question=question)
     if request.method == 'POST':
-        submission_time = datetime.now()
+        dt = datetime.now()
+        submission_time = dt.strftime('%Y-%m-%d %H:%M:%S')
         vote_number = 0
         questions = functions.display_question(id)
         question = questions[0]
@@ -79,7 +81,8 @@ def route_save_question():
     if request.method == 'GET':
         return render_template('add_question.html')
     if request.method == 'POST':
-        submission_time = datetime.now()
+        dt = datetime.now()
+        submission_time = dt.strftime('%Y-%m-%d %H:%M:%S')
         view_number = 0
         vote_number = 0
         title = request.form['title']
@@ -97,7 +100,8 @@ def add_new_comment(question_id):
         return render_template('new_comment.html', questions=questions, answers=answers)
     if request.method == 'POST':
         message = request.form['comment']
-        submission_time = datetime.now()
+        dt = datetime.now()
+        submission_time = dt.strftime('%Y-%m-%d %H:%M:%S')
         edited_count = 0
         functions.add_comment_to_question(question_id, message, submission_time, edited_count)
         return redirect(url_for('route_question', id=question_id))
@@ -111,7 +115,8 @@ def add_comment_to_answer(question_id, answer_id):
         return render_template('new_comment_to_answer.html', questions=questions, answers=answers)
     if request.method == 'POST':
         message = request.form['comment']
-        submission_time = datetime.now()
+        dt = datetime.now()
+        submission_time = dt.strftime('%Y-%m-%d %H:%M:%S')
         edited_count = 0
         functions.add_comment_to_answer(answer_id, message, submission_time, edited_count)
         return redirect(url_for('route_question', id=question_id, answer_id=answer_id))
@@ -149,7 +154,8 @@ def edit_comment(comment_id):
         return render_template('edit_comment.html', comment=comment)
     if request.method == 'POST':
         updated_message = request.form['message']
-        submission_time = datetime.now()
+        dt = datetime.now()
+        submission_time = dt.strftime('%Y-%m-%d %H:%M:%S')
         functions.update_comment(comment_id, updated_message, submission_time)
         return redirect(url_for('route_list'))
 
